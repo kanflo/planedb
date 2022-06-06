a121f04570b1b965a5063f5eebb5fca1caab2222
Johan Kanflo <johan.kanflo@bitfuse.net>
2022-06-06 20:50:00 +0200

    Fixed broken __init__.py

--- planedb/__init__.py
+++ planedb/__init__.py
@@ -1,1 +1,2 @@
-from .planedb import planedb
+from .planedb import init, lookup_aircraft_icao24, lookup_aircraft_registration, update_aircraft, lookup_airport, update_airport, lookup_airline, update_airline, lookup_route, update_route, add_imagecheck, delete_imagecheck, get_imagechecks
+
